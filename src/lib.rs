69d914f56a58b097584c0ce66e398e16a2beb6a8
SoundRabbit <soundrabbitstd@gmail.com>
2020-06-12 20:13:54 +0900

    サンプルコードをコピペ

--- src/lib.rs
+++ src/lib.rs
@@ -1,189 +1,30 @@
-extern crate nom;
-extern crate nom_recursive;
-
-use nom::{
-    branch::{alt, permutation},
-    bytes::complete::escaped_transform,
-    character::complete::{alpha1, anychar, char, multispace0, none_of},
-    combinator::{map, value},
-    multi::{many0, separated_list},
-    number::complete::double,
-    sequence::delimited,
-    IResult,
-};
+use nom::branch::*;
+use nom::character::complete::*;
+use nom::IResult;
 use nom_locate::LocatedSpan;
 use nom_recursive::{recursive_parser, RecursiveInfo};
 
+// Input type must implement trait HasRecursiveInfo
+// nom_locate::LocatedSpan<T, RecursiveInfo> implements it.
 type Span<'a> = LocatedSpan<&'a str, RecursiveInfo>;
 
-#[derive(Debug, PartialEq)]
-enum Literal {
-    Number(f64),
-    String(String),
-    Ident(String),
-    Reference(Vec<String>),
+pub fn expr(s: Span) -> IResult<Span, String> {
+    alt((expr_binary, term))(s)
 }
 
-fn number(s: Span) -> IResult<Span, Literal> {
-    map(double, |s| Literal::Number(s))(s)
-}
-
-fn string(s: Span) -> IResult<Span, Literal> {
-    map(
-        delimited(
-            char('\"'),
-            escaped_transform(
-                none_of("\"\\"),
-                '\\',
-                alt((
-                    value('\\', char('\\')),
-                    value('\"', char('\"')),
-                    value('\'', char('\'')),
-                    value('\r', char('r')),
-                    value('\n', char('n')),
-                    value('\t', char('t')),
-                )),
-            ),
-            char('\"'),
-        ),
-        |s| Literal::String(s),
-    )(s)
-}
-
-fn reference(s: Span) -> IResult<Span, Literal> {
-    map(
-        delimited(
-            char('{'),
-            separated_list(char('.'), many0(anychar)),
-            char('}'),
-        ),
-        |r| Literal::Reference(r.into_iter().map(|x| x.into_iter().collect()).collect()),
-    )(s)
-}
-
-fn literal(s: Span) -> IResult<Span, Literal> {
-    alt((number, string, reference))(s)
-}
-
-#[derive(Debug, PartialEq)]
-enum MultiplicativeOperator {
-    Multi,
-    Div,
-    Mod,
-}
-
-fn operator_multi(s: Span) -> IResult<Span, MultiplicativeOperator> {
-    map(char('*'), |_| MultiplicativeOperator::Multi)(s)
-}
-
-fn operator_div(s: Span) -> IResult<Span, MultiplicativeOperator> {
-    map(char('/'), |_| MultiplicativeOperator::Div)(s)
-}
-
-fn operator_mod(s: Span) -> IResult<Span, MultiplicativeOperator> {
-    map(char('%'), |_| MultiplicativeOperator::Mod)(s)
-}
-
-fn multiplicative_operator(s: Span) -> IResult<Span, MultiplicativeOperator> {
-    alt((operator_multi, operator_div, operator_mod))(s)
-}
-
-#[derive(Debug, PartialEq)]
-enum AdditiveOperator {
-    Add,
-    Minus,
-}
-
-fn operator_add(s: Span) -> IResult<Span, AdditiveOperator> {
-    map(char('+'), |_| AdditiveOperator::Add)(s)
-}
-
-fn operator_minus(s: Span) -> IResult<Span, AdditiveOperator> {
-    map(char('-'), |_| AdditiveOperator::Minus)(s)
-}
-
-fn additive_operator(s: Span) -> IResult<Span, AdditiveOperator> {
-    alt((operator_add, operator_minus))(s)
-}
-
-#[derive(Debug, PartialEq)]
-enum RelationalOperator {
-    Equals,
-    GreaterThan,
-    LessThan,
-    EqGreaterThan,
-    EqLessThan,
-    NotEquals,
-}
-
-fn operator_eq(s: Span) -> IResult<Span, RelationalOperator> {
-    map(permutation((char('='), char('='))), |_| {
-        RelationalOperator::Equals
-    })(s)
-}
-
-fn operator_gt(s: Span) -> IResult<Span, RelationalOperator> {
-    map(char('>'), |_| RelationalOperator::GreaterThan)(s)
-}
-
-fn operator_lt(s: Span) -> IResult<Span, RelationalOperator> {
-    map(char('>'), |_| RelationalOperator::LessThan)(s)
-}
-
-fn operator_eq_gt(s: Span) -> IResult<Span, RelationalOperator> {
-    map(permutation((char('>'), char('='))), |_| {
-        RelationalOperator::EqGreaterThan
-    })(s)
-}
-
-fn operator_eq_lt(s: Span) -> IResult<Span, RelationalOperator> {
-    map(permutation((char('<'), char('='))), |_| {
-        RelationalOperator::EqLessThan
-    })(s)
-}
-
-fn operator_not_eq(s: Span) -> IResult<Span, RelationalOperator> {
-    map(permutation((char('!'), char('='))), |_| {
-        RelationalOperator::NotEquals
-    })(s)
-}
-
-fn relational_operator(s: Span) -> IResult<Span, RelationalOperator> {
-    alt((
-        operator_eq,
-        operator_eq_gt,
-        operator_eq_lt,
-        operator_gt,
-        operator_lt,
-        operator_not_eq,
-    ))(s)
-}
-
-#[derive(Debug, PartialEq)]
-enum PrimaryExpression {
-    Tuple(Vec<Expression>),
-    Literal(Literal),
-}
-
+// Apply recursive_parser by custom attribute
 #[recursive_parser]
-fn tuple(s: Span) -> IResult<Span, Vec<Expression>> {
-    delimited(char('('), separated_list(char(','), expression), char(')'))(s)
+pub fn expr_binary(s: Span) -> IResult<Span, String> {
+    let (s, x) = expr(s)?;
+    let (s, y) = char('+')(s)?;
+    let (s, z) = expr(s)?;
+    let ret = format!("{}{}{}", x, y, z);
+    Ok((s, ret))
 }
 
-fn primary_expression(s: Span) -> IResult<Span, PrimaryExpression> {
-    alt((
-        map(literal, |x| PrimaryExpression::Literal(x)),
-        map(tuple, |x| PrimaryExpression::Tuple(x)),
-    ))(s)
-}
-
-#[derive(Debug, PartialEq)]
-enum Expression {
-    PrimaryExpression(PrimaryExpression),
-}
-
-fn expression(s: Span) -> IResult<Span, Expression> {
-    map(primary_expression, |x| Expression::PrimaryExpression(x))(s)
+pub fn term(s: Span) -> IResult<Span, String> {
+    let (s, x) = char('1')(s)?;
+    Ok((s, x.to_string()))
 }
 
 #[cfg(test)]
@@ -192,12 +33,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = expression(LocatedSpan::new_extra("(0,120.0,5)", RecursiveInfo::new()));
-        assert_eq!(
-            result.map(|(_, r)| r).ok(),
-            Some(Expression::PrimaryExpression(PrimaryExpression::Tuple(
-                vec![]
-            )))
-        );
+        let ret = expr(LocatedSpan::new_extra("1+1", RecursiveInfo::new()));
+        assert_eq!(ret.unwrap().1, String::from(""));
     }
 }
